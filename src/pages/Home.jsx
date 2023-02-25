30c052e1ea213819cb0e3c490747be65560a0d2c
Abhay <abhaypawar596@gmail.com>
2023-02-26 02:54:14 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      try {
+        const res = await axios.get(`/posts${cat}`);
+        console.log(res);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  // ];
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
+
+  return (
+
+    <div className="home">
+       <Tippy placement="bottom" content="follow on Twitter">
+        <a  style = {{marginRight:28,marginLeft:20, color: "gray" }} ><i className="fab fa-twitter fa-2x second"></i></a>
+      </Tippy>
+      <Tippy placement="bottom" content="follow on Facebook">
+        <a  style = {{marginRight:40, color: "gray" }} ><i className="fab fa-facebook fa-2x first"></i></a>
+      </Tippy>
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
+            </div>
+            <div className="content">
+              <Link className="linkPost" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              <p style = {{marginTop : 20, marginBottom : 30,color: "gray" }}>{getText(post.desc)}</p>
+              <button>Read More</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
