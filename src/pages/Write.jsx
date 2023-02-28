b67f9b9c82bb3b4d36fd01899c758f8965aa93f2
Abhay <abhaypawar596@gmail.com>
2023-02-28 10:33:07 +0530

    ..

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -27,23 +27,23 @@ const Write = () => {
 
   const handleClick = async (e) => {
      e.preventDefault();
-   // const imgUrl = await upload();
+    const imgUrl = await upload();
 
     try {
       state ? await axios.put(`https://blogapp-backend-qext.onrender.com/api/posts/${state.id}`, {
         title,
         descr: value,
         cat,
-        img: ""
        // img: file ? imgUrl : ""
+       img: imgUrl ? imgUrl : ""
       })
 
-        : await axios.post(`https://blogapp-backend-qext.onrender.com/api/posts/`, {
+        : await axios.post("https://blogapp-backend-qext.onrender.com/api/posts/", {
           title,
           descr: value,
           cat,
-         img:"",
-          //img: file ? imgUrl : "",
+        //  img: file ? imgUrl : ""
+          img: imgUrl ? imgUrl : "",
           date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
         });
       navigate("/");
